7c38508e359b1393e6c6e8c53caa0c6a284ffb05
🔐 Yagame <geraisusos25@gmail.com>
2025-04-21 08:19:38 -0300

    commit 0

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
+import random
 
-for i in range(10):
-    with open("data.txt", "a") as f:
-        f.write(f"Commit {i} - {datetime.now()}\n")
+total_commits = 200  # pode aumentar depois
+
+print("Iniciando...")
+
+for i in range(total_commits):
+    # dias aleatórios no passado
+    days_ago = random.randint(0, 365)
+    random_date = datetime.now() - timedelta(days=days_ago)
+
+    date_str = random_date.strftime("%Y-%m-%d %H:%M:%S")
+
+    with open("data.txt", "a", encoding="utf-8") as f:
+        f.write(f"Commit {i} - {date_str}\n")
 
     os.system("git add .")
-    os.system(f'git commit -m "commit {i}"')
 
+    # 🔥 IMPORTANTE: define data fake
+    os.system(f'git commit --date="{date_str}" -m "commit {i}"')
+
+print("Enviando...")
 os.system("git push")
+
+print("Finalizado!")
